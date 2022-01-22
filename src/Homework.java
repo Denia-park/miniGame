aa42d585758a9cb1669043a9265551ad6f6f1367
DeTop_My\Hyunggi <phg9587@naver.com>
2022-01-23 02:45:35 +0900

    - 난이도 기능 구현 (타이머 (난이도에 따라 시간이 다름) 에 따른 감점)
    - 타이머 쓰레드를 추가함으로써 생긴 공유자원에 관한 문제는 추후 수정예정

--- src/Homework.java
+++ src/Homework.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class userData{
+    public static final int Hard = 3;
+    public static final int Normal = 2;
+    public static final int Easy = 1;
+
     public static String answer;
     public static int gameScore;
+    public static int gameDifficulty;
     public static int correctAnswerScore = 3;
     public static int incorrectAnswerScore = -1;
     public static int standardScore = 30; // you have to be over this score to end Game
+
+    // public static String tempDiff;
 
     static Scanner scan = new Scanner(System.in);
 
@@ -15,6 +24,50 @@ class userData{
 
     public static void abandonRestOfUserTypingAnswer() {
         scan.nextLine();
+    }
+
+    public static boolean IsAnswerIntegerExcept0(){
+        return (Double.compare(Double.parseDouble(answer),Hard)  != 0 && Double.compare(Double.parseDouble(answer),Normal)  != 0 && Double.compare(Double.parseDouble(answer),Easy)  != 0);
+    }
+
+    public static void checkGameDifficulty(){
+        String tempDiff;
+
+        if(IsAnswerIntegerExcept0())
+        {
+            if(Double.parseDouble(answer) != 0)
+                System.out.println(answer + ": 소수점 입력은 버림으로 처리해서 진행합니다.");
+        }
+
+        gameDifficulty = (int)Math.floor(Double.parseDouble(answer));
+
+        if(gameDifficulty<1 || gameDifficulty>3)
+        {
+            System.out.println(answer + ": 잘못된 값을 입력하셨습니다.");
+            System.out.println("normal 난이도로 진행합니다.");
+            
+            gameDifficulty = Normal;
+            return;
+        }
+
+        if(gameDifficulty == Hard)
+        {
+            tempDiff = "Hard";
+        }
+        else if(gameDifficulty == Normal)
+        {
+            tempDiff = "Normal";
+        }
+        else if(gameDifficulty == Easy)
+        {
+            tempDiff = "Easy";
+        }
+        else
+        {
+            tempDiff = "";
+        }
+
+        System.out.println(tempDiff+" 난이도를 선택하셨습니다.");
     }
 
     public static void printUserTypingAnswer() {
@@ -55,10 +108,34 @@ class mainGame {
     static int questionSecondNumber = 0;
     static int questionAnswer = 0;
 
+    public static void setTimerByDiff(Timer ti) {
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Time Out : 1점 감점");
+                userData.minusUserScore();
+            }
+        };
+
+        if (userData.gameDifficulty == userData.Hard) {
+            ti.scheduleAtFixedRate(tt, 1100, 1000);
+        }
+        else if (userData.gameDifficulty == userData.Normal) {
+            ti.scheduleAtFixedRate(tt, 3100, 3000);
+        }
+        else if (userData.gameDifficulty == userData.Easy) {
+            ti.scheduleAtFixedRate(tt, 5100, 5000);
+        }
+    }
+
     public static void start() {
+
         userData.clearGameScore();
 
         while (isBelowStandardScore()) {
+            final Timer ti = new Timer();
+            setTimerByDiff(ti);
+
             questionFisrtNumber = getRandomValue();
             questionSecondNumber = getRandomValue();
 
@@ -78,8 +155,8 @@ class mainGame {
                 userData.minusUserScore();
             }
             userData.printUserScore();
+            ti.cancel();
         }
-
         System.out.println("30점 이상을 획득하셨습니다. 축하드립니다!.\n");
         checkUserWantToRestartGame();
     }
@@ -114,6 +191,14 @@ public class Homework {
         System.out.println("안녕하세요. 구구단 게임에 오신 것을 환영합니다.");
         System.out.println("게임 출제 범위는 2단 ~ 9단이며 사용자가 문제를 맞추실 경우 3점의 가점이, 틀리실 경우 1점의 감점이 있습니다.");
         System.out.println("사용자의 점수가 30점 이상이 될 경우, 게임이 끝나게 되며 이때 다시 게임 진행 여부를 확인합니다.\n");
+
+        System.out.println("해당 버전은 정답 입력시간에 제한을 건 버전입니다. 정답 입력시간으로 난이도 조절이 가능합니다.");
+        System.out.println("먼저 난이도 설정을 해주세요. ※Easy[5초] ,2: Normal[3초] ,3: Hard[1초]");
+        System.out.println("1: Easy ,2: Normal ,3: Hard 숫자 1,2,3 중 1개의 값을 입력해주세요.\n");
+
+        userData.scanUserTypingAnswer();
+        userData.abandonRestOfUserTypingAnswer();
+        userData.checkGameDifficulty();
 
         System.out.println("게임을 진행하시겠습니까 ? (Y / N)");
 
